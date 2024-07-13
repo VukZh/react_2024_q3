32b22f3a7fda1e17d0893fd103523e016ddee54f
VukZh <vukfx@inbox.ru>
2024-07-13 20:36:57 +0500

    feat: add not-found component, install react router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
 import Search from '../pages/search';
 import ErrorBoundary from '../shared/error-boundary';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from '../shared/not-found';
 
 function App() {
   return (
     <ErrorBoundary>
-      <Search />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Search />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </ErrorBoundary>
   );
 }
