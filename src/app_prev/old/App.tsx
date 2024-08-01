5cf16b31a160c07ade448300fababf3ce8d62206
VukZh <vukfx@inbox.ru>
2024-08-01 20:38:01 +0500

    chore: install NextJS

--- src/app_prev/old/App.tsx
+++ src/app_prev/old/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
-import Search from '../pages/search';
-import ErrorBoundary from '../shared/error-boundary';
+import Search from '../../pages/search';
+import ErrorBoundary from '../../shared/error-boundary';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import NotFound from '../shared/not-found';
-import { ContextProvider } from '../shared/context/contextProvider.tsx';
-import CharacterDetails from '../pages/search/entities/characterDetails';
-import { store } from '../shared/store/store.ts';
+import NotFound from '../../shared/not-found';
+import { ContextProvider } from '../../shared/context/contextProvider.tsx';
+import CharacterDetails from '../../pages/search/entities/characterDetails';
+import { store } from '../../shared/store/store.ts';
 import { Provider } from 'react-redux';
 
 function App() {
